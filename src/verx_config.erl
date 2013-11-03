b1ee41ad07213ce9b9a97c4ac58d20fffdcc02f6
Michael Santos <michael.santos@gmail.com>
2013-11-03 17:21:53 -0500

    verx_config: functions for updating libvirt XML
    Functions to manipulate libvirt configuration as Erlang terms and
    convert to XML.

---
+++ src/verx_config.erl
@@ -0,0 +1,114 @@
+%% Copyright (c) 2013, Michael Santos <michael.santos@gmail.com>
+%% All rights reserved.
+%%
+%% Redistribution and use in source and binary forms, with or without
+%% modification, are permitted provided that the following conditions
+%% are met:
+%%
+%% Redistributions of source code must retain the above copyright
+%% notice, this list of conditions and the following disclaimer.
+%%
+%% Redistributions in binary form must reproduce the above copyright
+%% notice, this list of conditions and the following disclaimer in the
+%% documentation and/or other materials provided with the distribution.
+%%
+%% Neither the name of the author nor the names of its contributors
+%% may be used to endorse or promote products derived from this software
+%% without specific prior written permission.
+%%
+%% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+%% "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+%% LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+%% FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+%% COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+%% INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+%% BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+%% LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+%% CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+%% LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+%% ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+%% POSSIBILITY OF SUCH DAMAGE.
+-module(verx_config).
+-include_lib("xmerl/include/xmerl.hrl").
+
+%%
+%% Convert between libvirt XML configuration and Erlang terms
+%%
+-export([
+        init/0, init/1,
+
+        set/2, set/3,
+        append/3,
+
+        to_xml/1
+    ]).
+
+init() ->
+    {domain, [], []}.
+
+init(Attr) ->
+    lists:foldl(fun(A,Cfg) -> set_attr(A, Cfg) end,
+        init(),
+        Attr).
+
+to_xml(Cfg) when is_tuple(Cfg) ->
+    lists:flatten(
+        xmerl:export_simple_content([Cfg], xmerl_xml)
+    ).
+
+set(Key, Val) when is_atom(Key) ->
+    {Key, maybe_iolist(Val)};
+set({Key, Attr}, Val) ->
+    {Key, Attr, maybe_iolist(Val)};
+set(Keys, Val) when is_list(Keys) ->
+    set_1(Keys, Val).
+
+set_1(Keys, Val) ->
+    {Node, Rest} = case lists:reverse(Keys) of
+        [{K,A}|T] ->
+            {{K,A,maybe_iolist(Val)}, T};
+        [H|T] ->
+            {{H,maybe_iolist(Val)}, T}
+    end,
+    set_2(Rest, Node).
+
+set_2([], Node) ->
+    Node;
+set_2([{Key, Attr}|T], Node) ->
+    set_2(T, {Key, Attr, [Node]});
+set_2([Key|T], Node) ->
+    set_2(T, {Key, [Node]}).
+
+set(Key, Val, {domain, _, _} = Cfg) ->
+    set_value(set(Key, Val), Cfg).
+
+append(Key, Val, {domain, _, _} = Cfg) ->
+    append_value(set(Key, Val), Cfg).
+
+set_value({Key, Val}, {domain, Attr, Content}) ->
+    {domain, Attr, set_opt({Key, Val}, Content)};
+set_value({Key, Opt, Val}, {domain, Attr, Content}) ->
+    {domain, Attr, set_opt({Key, Opt, Val}, Content)}.
+
+append_value({Key, Val}, {domain, Attr, Content}) ->
+    {domain, Attr, append_opt({Key, Val}, Content)}.
+
+set_attr({Key, Val}, {domain, Attr, Content}) ->
+    {domain, set_opt({Key, Val}, Attr), Content}.
+
+set_opt({Key, Val}, Options) ->
+    lists:keystore(Key, 1, Options, {Key, Val});
+set_opt({Key, Opt, Val}, Options) ->
+    lists:keystore(Key, 1, Options, {Key, Opt, Val}).
+
+append_opt({Key, Val}, Options) ->
+    case lists:keyfind(Key, 1, Options) of
+        false ->
+            set_opt({Key, Val}, Options);
+        {Key, OVal} ->
+            set_opt({Key, Val ++ OVal}, Options)
+    end.
+
+maybe_iolist([C|_] = N) when is_integer(C) -> [N];
+maybe_iolist(N) when is_list(N) -> N;
+maybe_iolist(N) -> [N].
---
+++ test/verx_config_tests.erl
@@ -0,0 +1,127 @@
+%% Copyright (c) 2013, Michael Santos <michael.santos@gmail.com>
+%% All rights reserved.
+%%
+%% Redistribution and use in source and binary forms, with or without
+%% modification, are permitted provided that the following conditions
+%% are met:
+%%
+%% Redistributions of source code must retain the above copyright
+%% notice, this list of conditions and the following disclaimer.
+%%
+%% Redistributions in binary form must reproduce the above copyright
+%% notice, this list of conditions and the following disclaimer in the
+%% documentation and/or other materials provided with the distribution.
+%%
+%% Neither the name of the author nor the names of its contributors
+%% may be used to endorse or promote products derived from this software
+%% without specific prior written permission.
+%%
+%% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+%% "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+%% LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+%% FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+%% COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+%% INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+%% BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+%% LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+%% CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+%% LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+%% ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+%% POSSIBILITY OF SUCH DAMAGE.
+-module(verx_config_tests).
+
+-compile(export_all).
+
+-include_lib("eunit/include/eunit.hrl").
+
+to_xml_test() ->
+    Cfg = verx_config:init([{type, ["lxc"]}]),
+    verx_config:to_xml(Cfg),
+    ok.
+
+name_test() ->
+    Cfg0 = verx_config:init([{type, ["qemu"]}]),
+    Name = verx_config:set(name, "test"),
+    {name, ["test"]} = Name,
+    Cfg = verx_config:set(name, "test", Cfg0),
+    XML = verx_config:to_xml(Cfg),
+    ?assertEqual(
+        "<domain type=\"qemu\"><name>test</name></domain>",
+        XML
+    ).
+
+interface_entry_test() ->
+    Cfg0 = verx_config:init([{type, ["qemu"]}]),
+    Cfg = verx_config:append([devices, {interface, [{type, "bridge"}]}], [
+            {mac, [{address, ["00:11:22:33:44:55"]}], []},
+            {source, [{bridge, "eth0"}], []}
+        ], Cfg0),
+    XML = verx_config:to_xml(Cfg),
+    ?assertEqual(
+        "<domain type=\"qemu\"><devices>"
+        "<interface type=\"bridge\"><mac address=\"00:11:22:33:44:55\"/>"
+        "<source bridge=\"eth0\"/></interface>"
+        "</devices></domain>",
+        XML).
+
+interface_list_test() ->
+    Cfg0 = verx_config:init(),
+    Iface1 = [
+        {mac, [{address, ["00:11:22:33:44:66"]}], []},
+        {source, [{bridge, ["eth1"]}], []}
+    ],
+    Iface2 = [
+        {mac, [{address, ["00:11:22:33:44:55"]}], []},
+        {source, [{bridge, ["eth0"]}], []}
+    ],
+    Cfg1 = verx_config:append([devices, {interface, [{type, "bridge"}]}], Iface1, Cfg0),
+    Cfg2 = verx_config:append([devices, {interface, [{type, "bridge"}]}], Iface2, Cfg1),
+    XML = verx_config:to_xml(Cfg2),
+    ?assertEqual(
+        "<domain><devices>"
+        "<interface type=\"bridge\"><mac address=\"00:11:22:33:44:55\"/>"
+        "<source bridge=\"eth0\"/></interface><interface type=\"bridge\">"
+        "<mac address=\"00:11:22:33:44:66\"/><source bridge=\"eth1\"/></interface>"
+        "</devices></domain>",
+        XML
+    ).
+
+filesystem_entry_test() ->
+    Cfg0 = verx_config:init(),
+    Cfg = verx_config:append([devices, {filesystem, [{type, "mount"}]}], [
+            {source, [{dir, ["/bin"]}], []},
+            {target, [{dir, ["/bin"]}], []},
+            readonly
+        ], Cfg0),
+    XML = verx_config:to_xml(Cfg),
+    ?assertEqual(
+        "<domain><devices>"
+        "<filesystem type=\"mount\"><source dir=\"/bin\"/>"
+        "<target dir=\"/bin\"/><readonly/></filesystem>"
+        "</devices></domain>",
+        XML
+    ).
+
+filesystem_list_test() ->
+    Cfg0 = verx_config:init(),
+    FS1 = [
+        {source, [{dir, ["/tmp/user"]}], []},
+        {target, [{dir, ["/home/user"]}], []}
+    ],
+    FS2 = [
+        {source, [{dir, ["/bin"]}], []},
+        {target, [{dir, ["/bin"]}], []},
+        readonly
+    ],
+    Cfg1 = verx_config:append([devices, {filesystem, [{type, "mount"}]}], FS1, Cfg0),
+    Cfg2 = verx_config:append([devices, {filesystem, [{type, "mount"}]}], FS2, Cfg1),
+    XML = verx_config:to_xml(Cfg2),
+    ?assertEqual(
+        "<domain><devices>"
+        "<filesystem type=\"mount\"><source dir=\"/bin\"/>"
+        "<target dir=\"/bin\"/><readonly/></filesystem>"
+        "<filesystem type=\"mount\">"
+        "<source dir=\"/tmp/user\"/><target dir=\"/home/user\"/></filesystem>"
+        "</devices></domain>",
+        XML
+    ).
